fe189892ee6eab3ef62a3388414805a593442e51
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-04-27 17:16:19 -0300

    Feat: add express server to heroku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/Layout/NavBar";
 import Footer from "./Components/Layout/Footer";
