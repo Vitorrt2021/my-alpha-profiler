c0ea30659cd9c01cbf61299d90b16bfc9067d142
Fabrício <fabri.rp2010@gmail.com>
2022-04-29 10:17:48 -0300

    home

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,8 +1,32 @@
 import { useLocation } from "react-router-dom";
 import Message from "../Layout/Message";
+import { useEffect, useState } from "react";
 import Nav from "../Layout/NavBar";
+
+const API_URL = "http://localhost:3003/user/current";
+
 function Home({ setToken }) {
   const location = useLocation();
+
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:3004",
+        "Access-Control-Allow-Credentials": true,
+      },
+      credentials: "include",
+    };
+    fetch(API_URL, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setUserData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
@@ -10,6 +34,12 @@ function Home({ setToken }) {
       {location.state?.message && (
         <Message message={location.state.message} type={location.state.type} />
       )}
+      <h2>{userData.username}</h2>
+      {/* Inserir imagem */}
+      <ul>
+        <li><strong>Email:</strong> {userData.email}</li>
+        <li><strong>Data de Nascimento:</strong> {userData.data}</li>
+      </ul>
     </div>
   );
 }
