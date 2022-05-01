754e05917b814e9b9f861887b0575dd254bb233e
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-05-01 11:02:35 -0300

    Fix; merge to github

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -4,20 +4,23 @@ import { useEffect, useState } from "react";
 import styles from "./Home.module.css";
 import Nav from "../Layout/NavBar";
 
-const API_URL = "http://localhost:3003/user/current";
+const API_URL = "https://my-alpha-profiler-backend.herokuapp.com/user/current";
 
 function Home({ setToken }) {
   const location = useLocation();
 
-  const [userData, setUserData] = useState({birthdate : ""});
+  const [userData, setUserData] = useState({ birthdate: "" });
 
-  const img = (userImg) => {if (userImg != null) return userImg; else return "https://www.seekpng.com/png/full/356-3562377_personal-user.png"}
+  const img = (userImg) => {
+    if (userImg != null) return userImg;
+    else
+      return "https://www.seekpng.com/png/full/356-3562377_personal-user.png";
+  };
 
   const date = (birth) => {
     const info = birth.split("T")[0].split("-");
-    return info[2]+"/"+info[1]+"/"+info[0];
-  }
-  
+    return info[2] + "/" + info[1] + "/" + info[0];
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -41,15 +44,22 @@ function Home({ setToken }) {
     <div className={styles.container}>
       <div>
         {location.state?.message && (
-          <Message message={location.state.message} type={location.state.type} />
+          <Message
+            message={location.state.message}
+            type={location.state.type}
+          />
         )}
       </div>
       <div className={styles.contend_container}>
         <h2>{userData.username}</h2>
-        <img src={img (userData.user_img)} />
+        <img src={img(userData.user_img)} />
         <ul>
-          <li><strong>Email:</strong> {userData.email}</li>
-          <li><strong>Data de Nascimento:</strong> {date(userData.birthdate)}</li>
+          <li>
+            <strong>Email:</strong> {userData.email}
+          </li>
+          <li>
+            <strong>Data de Nascimento:</strong> {date(userData.birthdate)}
+          </li>
         </ul>
       </div>
     </div>
